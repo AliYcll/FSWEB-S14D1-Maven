cc9e17f0de12288519d8170d14a25f7a787dc84d
Ali Yücel <ali.yucel675@hotmail.com>
2025-11-06 03:24:27 +0300

    proje tamamlandı

---
+++ src/main/java/com/workintech/developers/HRManager.java
@@ -0,0 +1,50 @@
+package com.workintech.developers;
+
+public class HRManager extends Employee {
+    private JuniorDeveloper[] juniorDevelopers;
+    private MidDeveloper[] midDevelopers;
+    private SeniorDeveloper[] seniorDevelopers;
+
+    public HRManager(long id, String name, double salary) {
+        super(id, name, salary);
+        this.juniorDevelopers = new JuniorDeveloper[10];
+        this.midDevelopers = new MidDeveloper[10];
+        this.seniorDevelopers = new SeniorDeveloper[10];
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName() + " starts to working as HR Manager");
+        setSalary(getSalary() * 1.5);
+    }
+
+    public void addEmployee(JuniorDeveloper juniorDeveloper) {
+        for (int i = 0; i < juniorDevelopers.length; i++) {
+            if (juniorDevelopers[i] == null) {
+                juniorDevelopers[i] = juniorDeveloper;
+                return;
+            }
+        }
+        System.out.println("Junior developer array is full");
+    }
+
+    public void addEmployee(MidDeveloper midDeveloper) {
+        for (int i = 0; i < midDevelopers.length; i++) {
+            if (midDevelopers[i] == null) {
+                midDevelopers[i] = midDeveloper;
+                return;
+            }
+        }
+        System.out.println("Mid developer array is full");
+    }
+
+    public void addEmployee(SeniorDeveloper seniorDeveloper) {
+        for (int i = 0; i < seniorDevelopers.length; i++) {
+            if (seniorDevelopers[i] == null) {
+                seniorDevelopers[i] = seniorDeveloper;
+                return;
+            }
+        }
+        System.out.println("Senior developer array is full");
+    }
+}
